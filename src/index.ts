2080c8dea18dca53c127d566ee1c907b47b11e6a
James Keveren <james@keve.ren>
2022-03-25 21:21:34 +0000

    reorganised API

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-interface SpotifyOptions {
+// config for Spotify constructor
+interface SpotifyConfig {
 	baseURL: string;
 	clientId: string;
 	redirectURL: string;
@@ -7,50 +8,61 @@ interface SpotifyOptions {
 }
 
 export class Spotify {
-	options: SpotifyOptions
+	config: SpotifyConfig
 
-	constructor(options: SpotifyOptions) {
-		this.options = options;
+	_requester: Function
+
+	constructor(config: SpotifyConfig) {
+		this.config = config;
+		// this._requester = http | https
 	}
 
 	// https://developer.spotify.com/documentation/general/guides/authorization/code-flow/#request-user-authorization
 	makeOAuthURL(): URL {
-		const u = new URL(this.options.baseURL);
-		u.searchParams.set("client_id", this.options.clientId);
+		const u = new URL(this.config.baseURL);
+		u.searchParams.set("client_id", this.config.clientId);
 		u.searchParams.set("response_type", "code");
-		u.searchParams.set("redirect_uri", this.options.redirectURL);
+		u.searchParams.set("redirect_uri", this.config.redirectURL);
 		u.searchParams.set("state", ""); // not sure how to handle this yet
-		u.searchParams.set("scope", this.options.scopes.join(" "));
-		u.searchParams.set("show_dialog", this.options.showDialog.toString());
+		u.searchParams.set("scope", this.config.scopes.join(" "));
+		u.searchParams.set("show_dialog", this.config.showDialog.toString());
 		return u;
 	}
 
 	// Creates a user from auth code
-	getUser(code: string): SpotifyUser {
+	async getUser(code: string): Promise<SpotifyUser> {
+		// make request to get refresh token and initial access token
+		// https://developer.spotify.com/documentation/general/guides/authorization/code-flow/#request-access-token
+		// construct and return SpotifyUser
 		return new SpotifyUser()
 	}
 }
 
 export class SpotifyUser {
 	spotify: Spotify
-	authCode: string
 	accessToken: string
 	accessTokenExpiryTime: Date
 	refreshToken: string
 
-	// Get this user's access token
-	// https://developer.spotify.com/documentation/general/guides/authorization/code-flow/#request-access-token
-	// requires authCode
-	async getAccessToken(): Promise<void> {
+	// internal request function to use
+	// mutable for mocking
+	_requester: Function
+
+	constructor() {
+		// this._requester = http | https
 	}
 
 	// Refresh this users access token
-	// https://developer.spotify.com/documentation/general/guides/authorization/code-flow/#request-a-refreshed-access-token
 	async refreshAccessToken(): Promise<void> {
+		//make refresh request
+		//https://developer.spotify.com/documentation/general/guides/authorization/code-flow/#request-a-refreshed-access-token
 	}
 
 	// make request to spotify api
-	// This method handles rate limits
+	// This method handles rate limits and access token refreshing.
 	async request() {
+		// get refresh token if neccessary
+		// make request
+		// this.requester()
 	}
 }
